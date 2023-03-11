ffc2d7d4eab8067541a5ac2d672425bc03178e3b
Andres Felipe <pizoluligoa@gmail.com>
2023-03-11 17:01:03 -0500

    npm 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <link rel="stylesheet" href="{{ asset('build/assets/app-d9704236.css') }}">
-        <script src="{{ asset('build/assets/app-43c05ea5.js') }}"></script>
+        {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-d9704236.css') }}">
+        <script src="{{ asset('build/assets/app-43c05ea5.js') }}"></script> --}}
 
 
         <!-- Styles -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-d9704236.css') }}">
+        <script src="{{ asset('build/assets/app-43c05ea5.js') }}"></script> --}}
     </head>
     <body>
         <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
