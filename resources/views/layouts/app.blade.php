ac40e6f56744b63fd518fc1f2269165b7ff6326f
Andres Felipe <pizoluligoa@gmail.com>
2023-03-11 16:49:41 -0500

    npm 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,10 @@
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
+        <link rel="stylesheet" href="{{ asset('build/assets/app-d9704236.css') }}">
+        <script src="{{ asset('build/assets/app-43c05ea5.js') }}"></script>
+
+
         <!-- Styles -->
         @livewireStyles
     </head>
@@ -45,5 +49,6 @@
         @stack('modals')
 
         @livewireScripts
+
     </body>
 </html>
